1b616ebbc44a726ced0a52c3e63c943bf9ca88e8
= <=>
2015-08-18 14:08:56 +0000

    added web server and multiprocessing

--- code/main.py
+++ code/main.py
@@ -26,6 +26,8 @@ import os
 import time
 import RPi.GPIO as GPIO
 from PWM_driver import PWM
+from flask import Flask, jsonify, render_template, request
+import multiprocessing as mp
 
 
 # ============================================================================
@@ -176,12 +178,14 @@ def getFrame():
     # print frame16
     return frame16
 
-def setVibration():
+def setVibration(frame = None):
 
     """ Sets PWM values to each vibration motor unit. """
 
+    if frame is None:
+        frame = getFrame()
     mappingFunction = 8 # Mapping of grayscale values to PWM values.
-    PWM16 = mappingFunction * getFrame()
+    PWM16 = mappingFunction * frame
     maxPWM = 3060
     for row in range(0,8):
         for col in range (0,16):
@@ -196,74 +200,165 @@ def setVibration():
 
 
 # ============================================================================
-# Main function
+# Vibration renderer process
 # ============================================================================
 
+def rendererProcess(webQueue, ipcQueue):
+
+    """ Entry point for the renderer process. """
+
+    # Initialization of PWM drivers. PWM(0x40, debug=True) for debugging.
+    global IC
+    IC = []
+    freq = 490
+    for i in range(0,8):
+        IC.append(PWM(0x40+i))
+        IC[i].setPWMFreq(freq)
+
+    # Initialization of the sensor.
+    global sensor
+    sensor = CV_CAP_OPENNI_ASUS
+    global channel
+    channel = 3
+    global gain
+    gain = 5
+    global capture
+    capture = cv2.VideoCapture(sensor)
+    capture.open(sensor)
+    while not capture.isOpened():
+        print "Couldn't open sensor. Is it connected?"
+        time.sleep(100)
+    print "Sensor opened successfully"
+
+    # GPIO initialization.
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    sw1 = GPIO.input(18) # Input NO switch.
+    flag = 0 # Flag for start/stop button.
+
+    # Waits for sw1 to be pressed.
+    print "System ready, press switch to continue..."
+    beep(1, 0.2)
+    GPIO.wait_for_edge(18, GPIO.RISING)
+    fadeIn()
+
+    shouldTerminate = False
+
+    global sourceMode
+    sourceMode = "kinect"
+    
+    while not shouldTerminate:
+        if ((GPIO.input(18) == False) or
+            (GPIO.input(18) == True and flag == 1)):
+            flag = 0
+            tick = time.clock()
+
+            # depthTest()
+            # sweepTest()
+            # strobeTest(0)
+            # getFrame()
+            if not webQueue.empty():
+                requestJson = webQueue.get()
+                sourceMode = requestJson["mode"]
+
+            if sourceMode == "web":
+                frame = np.array(requestJson["motors"])
+                setVibration(frame)
+                print "web mode"
+            elif sourceMode == "kinect":
+                setVibration()
+                print "kinect mode"
+            
+
+            tock = time.clock()
+            runtime = tock - tick
+            print "Loop runtime: " + str(runtime) + "s"
+            print "FPS: " + str(int(1/runtime))
+
+            if not ipcQueue.empty():
+                ipcCommand = ipcQueue.get()
+                if ipcCommand == "terminate":
+                    shouldTerminate = True
+
+        else:
+            pause()
+
+    print "[Renderer] shutdown requested"
+    GPIO.cleanup()
+    for i in range(0, 8):
+        IC[i].setAllPWM(0, 0)
+    
+    print "[Renderer] successfully shutdown"
+
+
+# ============================================================================
+# Web server process
+# ============================================================================
+webServer = Flask(__name__)
+webServer.debug = True
+
+
+@webServer.route("/")
+def serve():
+    """ Web root. """
+    return render_template('index.html')
+
+
+@webServer.route('/_send_motors', methods=["PUT", "POST"])
+def send_motors():
+
+    """ Handles AJAX requests. """
+
+    requestJson = request.get_json()
+    webServer.extensions['queue'].put(requestJson)
+
+    retVal = {"message": "hi from Flask"}
+    return jsonify(**retVal)
+
+
+def webserverProcess(webQueue, ipcQueue):
+
+    """ Entry point for the web server process. """
+    if not hasattr(webServer, 'extensions'):
+        webServer.extensions = {}
+    webServer.extensions['queue'] = webQueue
+
+    webServer.run(host='0.0.0.0', use_reloader=False)
+
+    while True:
+        ipcCommand = ipcQueue.get()
+        if ipcCommand == "terminate":
+            print "[WebServer] shutdown requested"
+            return
+            
+
+# ============================================================================
+# Main function
+# ============================================================================
 def main():
 
+    # for inter-process communication
+    webQueue = mp.Queue()
+    ipcQueue = mp.Queue()
+
+    # web server process
+    p_webserver = mp.Process(target=webserverProcess, args=(webQueue, ipcQueue))
+    p_webserver.start()
+
+    # jacket renderer process
+    p_renderer = mp.Process(target=rendererProcess, args=(webQueue, ipcQueue))
+    p_renderer.start()
+
+    workers = [p_webserver, p_renderer]
     try:
-        # Initialization of PWM drivers. PWM(0x40, debug=True) for debugging.
-        global IC
-        IC = []
-        freq = 490 
-        for i in range(0,8):
-            IC.append(PWM(0x40+i))
-            IC[i].setPWMFreq(freq)
-
-        # Initialization of the sensor.
-        global sensor
-        sensor = CV_CAP_OPENNI_ASUS
-        global channel
-        channel = 3
-        global gain
-        gain = 5
-        global capture
-        capture = cv2.VideoCapture(sensor)
-        capture.open(sensor)
-        while not capture.isOpened():
-            print "Couldn't open sensor. Is it connected?"
-            time.sleep(100)
-        print "Sensor opened successfully"
-
-        # GPIO initialization.
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        sw1 = GPIO.input(18) # Input NO switch.
-        flag = 0 # Flag for start/stop button.
-
-        # Waits for sw1 to be pressed.
-        print "System ready, press switch to continue..."
-        beep(1, 0.2)
-        GPIO.wait_for_edge(18, GPIO.RISING)
-        fadeIn()
-
-        while True:
-            if ((GPIO.input(18) == False) or
-                (GPIO.input(18) == True and flag == 1)):
-                flag = 0
-                tick = time.clock()
-
-                # depthTest()
-                # sweepTest()
-                # strobeTest(0)
-                # getFrame()
-                setVibration()
-
-                tock = time.clock()
-                runtime = tock - tick
-                print "Loop runtime: " + str(runtime) + "s"
-                print "FPS: " + str(int(1/runtime))
-            else:
-                pause()
-
+        for worker in workers:
+            worker.join()
     except KeyboardInterrupt:
-        GPIO.cleanup
-        for i in range(0, 8):
-            IC[i].setAllPWM(0, 0)
-        print "Shutdown requested. \nExiting..."
+        print "[Main] received Ctrl + C"
+        ipcQueue.put("terminate")
+        for worker in workers:
+            worker.join()
 
-    finally:
-        print "Done"
 
 if __name__ == "__main__":
     main()
